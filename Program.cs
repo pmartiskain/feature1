adcad4efd30c76b9b33d89b490ae9fb5afe0a23c
Petja Martiskainen <petja.martiskainen@norcloud.com>
2023-12-10 17:18:08 +0200

    Feature 2 added to Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+// Test
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
